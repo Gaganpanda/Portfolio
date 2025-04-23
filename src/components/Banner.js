c6927da35eeacfd7f8130bb12e627a2a6600e3ec
Gagan Pandey <157130627+Gaganpanda@users.noreply.github.com>
2025-04-24 00:35:15 +0530

    vbc

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -57,7 +57,9 @@ export const Banner = () => {
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                 <span className="tagline">Welcome to my Portfolio</span>
                 <h1>{`Hi! I'm Gagan`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Software Engineer", "Web Developer", "Software Tester" ]'><span className="wrap">{text}</span></span></h1>
-                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                  <p>Hi, I'm Gagan Pandey – a passionate Software Engineer with a strong interest in Competitive Programming, Web Development, and AI/ML. I love solving real-world problems through code and continuously explore new technologies to enhance my skills and build impactful projects.
+
+</p>
                   <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
               </div>}
             </TrackVisibility>
